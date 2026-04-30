a2ada022fb832b36931989b4995c6a512c99fe44
Utsav Kakdiya <utsavkakdiya8@gmail.com>
2026-04-30 11:49:06 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,8 +4,10 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
   const phone = document.getElementById("phone").value.trim();
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
-  const confirm_password = document.getElementById("confirm-password").value.trim();
-  const privacy_checkbox = document.getElementById("privacy-policy")
+  const confirm_password = document
+    .getElementById("confirm-password")
+    .value.trim();
+  const privacy_checkbox = document.getElementById("privacy-policy");
   const errorMessage = document.getElementById("error-message");
   errorMessage.textContent = "";
 
@@ -14,15 +16,15 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
     return;
   }
 
-  if(phone === ""){
-    errorMessage.textContent = "Phone number is required."
-    return
+  if (phone === "") {
+    errorMessage.textContent = "Phone number is required.";
+    return;
   }
 
-  const phoneRegex = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/
-  if (!phoneRegex.test(phone)){
-    errorMessage.textContent = "Enter a valid phone number."
-    return
+  const phoneRegex = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/;
+  if (!phoneRegex.test(phone)) {
+    errorMessage.textContent = "Enter a valid phone number.";
+    return;
   }
 
   if (email === "") {
@@ -41,9 +43,9 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
     return;
   }
 
-  if(confirm_password !== password){
-    errorMessage.textContent = "Password and Confirm Password must be same."
-    return
+  if (confirm_password !== password) {
+    errorMessage.textContent = "Password and Confirm Password must be same.";
+    return;
   }
 
   if (!privacy_checkbox.checked) {
@@ -55,7 +57,6 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
 });
 
 function sendDataToDatabase(data) {
-
   displayLoader("Sending OTP...");
 
   fetch("/register", {
@@ -69,13 +70,12 @@ function sendDataToDatabase(data) {
       hideLoader();
 
       if (result.success) {
-        // To hide the details fileds 
+        // To hide the details fileds
         let signUp = document.getElementById("signupSection");
         signUp.style.display = "none";
 
-
-        // To display otp field 
-        let otpSection = document.getElementById("otpverification")
+        // To display otp field
+        let otpSection = document.getElementById("otpverification");
         otpSection.style.display = "block";
 
         // Set hidden email input for OTP verification
@@ -83,7 +83,6 @@ function sendDataToDatabase(data) {
 
         msg.style.color = "green";
         msg.textContent = result.message;
-
       } else {
         msg.style.color = "red";
         msg.textContent = result.message;
@@ -95,51 +94,49 @@ function sendDataToDatabase(data) {
     });
 }
 
-document.getElementById("submitOtp").addEventListener("click", async function (e) {
-  e.preventDefault();
+document
+  .getElementById("submitOtp")
+  .addEventListener("click", async function (e) {
+    e.preventDefault();
 
-  const otp = document.getElementById("OTP").value.trim();
-  const email = document.getElementById("otpEmail").value; 
-  const msg = document.getElementById("error-message");
-  msg.textContent = "";
+    const otp = document.getElementById("OTP").value.trim();
+    const email = document.getElementById("otpEmail").value;
+    const msg = document.getElementById("error-message");
+    msg.textContent = "";
 
-  if (!otp) {
-    msg.style.color = "red";
-    msg.textContent = "Enter OTP";
-    return;
-  }
-  try{
+    if (!otp) {
+      msg.style.color = "red";
+      msg.textContent = "Enter OTP";
+      return;
+    }
+    try {
+      displayLoader("Verifying OTP...");
 
-    displayLoader("Verifying OTP...")
+      const res = await fetch("/verify-otp", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, otp }),
+      });
 
-    const res = await fetch("/verify-otp", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, otp }),
-    });
-    
-    const data = await res.json();
-    hideLoader()
+      const data = await res.json();
+      hideLoader();
 
       localStorage.setItem("userEmail", email);
-    if (data.success) {
-      msg.style.color = "green";
-      msg.textContent = data.message;
+      if (data.success) {
+        msg.style.color = "green";
+        msg.textContent = data.message;
 
-      
-      // Redirect after OTP verification
-      setTimeout(() => {
-        displayLoader()
-        window.location.href = "home.html";
-      }, 1500);
-      hideLoader()
-    } else {
-      msg.style.color = "red";
-      msg.textContent = data.message;
+        // Redirect after OTP verification
+        setTimeout(() => {
+          displayLoader();
+          window.location.href = "home.html";
+        }, 1500);
+        hideLoader();
+      } else {
+        msg.style.color = "red";
+        msg.textContent = data.message;
+      }
+    } catch (err) {
+      console.error(err);
     }
-  }catch (err) {
-    console.error(err);
-  }
-});
-  
-  
+  });
