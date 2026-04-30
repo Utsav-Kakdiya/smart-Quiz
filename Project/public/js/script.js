6c18008906ce2e61cad5ca3ddc7344510ed35674
Utsav Kakdiya <utsavkakdiya8@gmail.com>
2026-04-30 10:42:21 +0530

    Delete Project directory

--- Project/public/js/script.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll(".nav-links a");
-const currentPage = window.location.pathname.split("/").pop();
-
-links.forEach((link) => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
-  }
-});
